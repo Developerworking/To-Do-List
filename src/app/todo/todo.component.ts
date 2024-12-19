c814e53c5b1e8db13834dc355dca0e8cbfe225b6
MANDEEP SINGH OBEROI <mo4335@srmist.edu.in>
2024-12-19 11:56:22 +0530

    todo

---
+++ src/app/todo/todo.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  standalone: true, // Ensure this is true if using standalone components
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.css'],
+  imports: [CommonModule, FormsModule] 
+})
+export class TodoComponent {
+  tasks: string[] = [];
+  newTask: string = '';
+
+  addTask() {
+    if (this.newTask.trim()) {
+      this.tasks.push(this.newTask.trim());
+      this.newTask = '';
+    }
+  }
+
+  removeTask(index: number) {
+    this.tasks.splice(index, 1);
+  }
+}
+
